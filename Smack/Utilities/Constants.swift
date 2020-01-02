5b398d018db6f3ea1b4ccd387ff05f3ae9595a0d
Jukess <juancaluque@hotmail.es>
2020-01-02 12:33:40 -0600

    Creating a web request done

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,23 @@
 
 import Foundation
 
-//segues
+
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//URL Constants
+let BASE_URL = "https://damp-forest-50249.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+//Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccountVC"
 let UNWIND = "unwindToChannel"
+
+//User Defaults
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+
+
