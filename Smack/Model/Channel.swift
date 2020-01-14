6b24bcf5378e626c0c764c08dfc117bd5a2a8d8a
Jukess <juancaluque@hotmail.es>
2020-01-13 18:11:52 -0600

    Getting channel data

---
+++ Smack/Model/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Juan Luque on 1/13/20.
+//  Copyright © 2020 Juan Luque. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle : String!
+    public private(set) var channelDescription : String!
+    public private(set) var id : String!
+}
